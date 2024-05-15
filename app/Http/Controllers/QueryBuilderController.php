8229cc790adc63274b76c42d7ee3afe5629c68d2
Júnior <juniorgranjeiro@hotmail.com>
2024-05-15 15:33:45 -0300

    Inserido registro do banco

--- app/Http/Controllers/QueryBuilderController.php
+++ app/Http/Controllers/QueryBuilderController.php
@@ -11,9 +11,18 @@ class QueryBuilderController extends Controller
         {
 
             // Verificação de registros no banco de dados
-            $users = DB::table('users')->get();
-            dd(DB::table('users')->get());
-         
+            // $users = DB::table('users')->get();
+            // dd(DB::table('users')->get());
+        
+            // Inserindo registro no banco de dados
+            $insere = DB::table('users')->insert([
+                            [
+                            'name' => 'Matheus Fellipe',
+                            'email' => 'matheus.fellipe@mail.com',
+                            'password' => bcrypt('123456')
+                            ],
+                        ]);
+                        dd($insere);
     
         }
 
